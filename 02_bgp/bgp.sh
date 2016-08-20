59a374a52d0e01bca89a8c762870851796f23d83
root <root@rails01.kazutan.com>
2016-08-21 00:48:30 +0900

    changed script pathy

--- 02_bgp/bgp.sh
+++ 02_bgp/bgp.sh
@@ -1,9 +1,9 @@
-ruby ../00_rollback-to-init.rb && \
-ruby 01_bgp-lab-set-interface.rb && \
-ruby 02_bgp-lab-set-eBGP-iBGP.rb && \
-ruby 03_bgp-lab-set-network-and-ospf.rb && \
-ruby 04_bgp-lab-set-route_reflector.rb && \
-ruby 05_bgp-lab-set-peer-group.rb && \
-ruby 06_bgp-lab-set-weight.rb && \
-ruby 07_bgp-lab-set-LP.rb && \
-ruby 08_bgp-lab-set-AS-prepend.rb 
+/root/.rbenv/shims/ruby /root/script/autoconfig/00_rollback-to-init.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/02_bgp/01_bgp-lab-set-interface.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/02_bgp/02_bgp-lab-set-eBGP-iBGP.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/02_bgp/03_bgp-lab-set-network-and-ospf.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/02_bgp/04_bgp-lab-set-route_reflector.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/02_bgp/05_bgp-lab-set-peer-group.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/02_bgp/06_bgp-lab-set-weight.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/02_bgp/07_bgp-lab-set-LP.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/02_bgp/08_bgp-lab-set-AS-prepend.rb 
