ff4925e745b80b95554e1c4a5c400fac7d4088a1
root <root@rails01.kazutan.com>
2016-08-20 23:22:42 +0900

    edit script path

--- 01_ospf/ospf.sh
+++ 01_ospf/ospf.sh
@@ -1,12 +1,12 @@
-ruby ../00_rollback-to-init.rb && \
-ruby 01_ospf-lab-set-interface.rb && \
-ruby 02_ospf-lab-set-ospf.rb && \
-ruby 03_ospf-redis-connected.rb && \
-ruby 04_ospf-tshift-R3.rb && \
-ruby 05_ospf-demand-circuit-and-flood-reduction.rb && \
-ruby 06_ospf-sha-auth.rb && \
-ruby 07_ospf-basic-default-route.rb  && \
-ruby 08_ospf-advanced-default-route.rb  && \
-ruby 09_ospf-change-area1-to-stub.rb && \
-ruby 10_ospf-change-area2-to-nssa.rb && \
-ruby 11_ospf-remove-stub-nssa.rb
+/root/.rbenv/shims/ruby /root/script/autoconfig/00_rollback-to-init.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/01_ospf/01_ospf-lab-set-interface.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/01_ospf/02_ospf-lab-set-ospf.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/01_ospf/03_ospf-redis-connected.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/01_ospf/04_ospf-tshift-R3.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/01_ospf/05_ospf-demand-circuit-and-flood-reduction.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/01_ospf/06_ospf-sha-auth.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/01_ospf/07_ospf-basic-default-route.rb  && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/01_ospf/08_ospf-advanced-default-route.rb  && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/01_ospf/09_ospf-change-area1-to-stub.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/01_ospf/10_ospf-change-area2-to-nssa.rb && \
+/root/.rbenv/shims/ruby /root/script/autoconfig/01_ospf/11_ospf-remove-stub-nssa.rb
